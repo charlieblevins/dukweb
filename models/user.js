f0325375972ac8add4d7209c0ef5af1b5fa2e86a
Charlie Blevins <blevins.charlie@gmail.com>
2015-12-23 08:33:38 -0500

    added password confirm. set email as username

--- models/user.js
+++ models/user.js
@@ -2,8 +2,5 @@ var mongoose = require('mongoose');
 
 module.exports = mongoose.model('User', {
     username: String,
-    password: String,
-    email: String,
-    gender: String,
-    address: String
+    password: String
 });
--- passport/signup.js
+++ passport/signup.js
@@ -5,53 +5,49 @@ var bCrypt = require('bcrypt-nodejs');
 module.exports = function (passport) {
 
     passport.use('signup', new LocalStrategy({
+        usernameField: 'email',
         passReqToCallback: true
     },
     function (req, username, password, done) {
 
-        findOrCreateUser = function () {
-            // find user in mongo
-            console.log('validating sign up...');
-            User.findOne({'username': username}, function (err, user) {
-                console.log('findOne for signup routine completed. handling response from mongo');
+        // find user in mongo
+        console.log('validating sign up...');
+        User.findOne({'username': username}, function (err, user) {
+            console.log('findOne for signup routine completed. handling response from mongo');
 
-                // In case of any error, return using the done method
-                if (err) {
-                    console.log('Error in signup: ' + err);
-                    return done(err);
-                }
+            // In case of any error, return using the done method
+            if (err) {
+                console.log('Error in signup: ' + err);
+                return done(err);
+            }
 
-                // already exists
-                if (user) {
-                    console.log('User already exists with username: ' + username);
-                    return done(null, false, req.flash('message', 'User already exists'));
-                } else {
-                    // If there is no user with that email
-                    // create the user
-                    var newUser = new User();
+            // already exists
+            if (user) {
+                console.log('User already exists with username: ' + username);
+                return done(null, false, req.flash('message', 'User already exists'));
+            } else if (password !== req.param('confirm-pass')) {
+                console.log('Passwords do not match.');
+                return done(null, false, req.flash('message', 'Passwords must match'));
+            } else {
+                // If there is no user with that email
+                // create the user
+                var newUser = new User();
 
-                    // set the user's local credentials
-                    newUser.username = username;
-                    newUser.password = createHash(password);
-                    newUser.email = req.param('email');
-                    newUser.firstName = req.param('firstName');
-                    newUser.lastName = req.param('lastName');
+                // set the user's local credentials
+                newUser.username = username;
+                newUser.password = createHash(password);
 
-                    // save the user
-                    newUser.save(function(err) {
-                        if (err) {
-                            console.log('Error in saving user: ' + err);
-                            throw err;
-                        }
-                        console.log('User registration successful');
-                        return done(null, newUser);
-                    });
-                }
-            });
-        };
-
-        // Execute in next tick of event loop
-        process.nextTick(findOrCreateUser);
+                // save the user
+                newUser.save(function(err) {
+                    if (err) {
+                        console.log('Error in saving user: ' + err);
+                        throw err;
+                    }
+                    console.log('User registration successful');
+                    return done(null, newUser, req.flash('message', 'Registration successful!'));
+                });
+            }
+        });
     }));
 
     // Generates hash using bCrypt
