c27badfcf3814478916a103419d845c7ca51ebc9
root <root@ip-172-31-32-200.ec2.internal>
2016-06-26 20:09:53 +0000

    message login to ios

--- public/scripts/messenger.js
+++ public/scripts/messenger.js
@@ -1,1 +1,39 @@
-console.log('messenger.js');
+(function () {
+
+    if (!inIOS()) return false; 
+
+	// When a user signs in - send a message to the ios app
+	onSignIn(sendSignInMessage);
+
+
+	function onSignIn (callback) {
+		var si_btn = document.querySelector('button[type="submit"]');	
+		si_btn.addEventListener('click', sendSignInMessage);
+	}
+
+	function sendSignInMessage () {
+
+		if (!window.webkit.messageHandlers.signInClicked) return false
+
+		var username = document.querySelector('input[name="username"]').value;
+		var password = document.querySelector('input[name="password"]').value;
+
+		var messageToPost = {
+			'username': username,
+			'password': password
+		};
+		window.webkit.messageHandlers.signInClicked.postMessage(messageToPost);
+	}
+
+	/**
+	 * Returns true if in ios, false if not
+	 */
+	function inIOS () {
+		if (window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+})();
