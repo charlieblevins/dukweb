62cc5580e89b45569d08a2fd76f42f03c2ede467
Charlie Blevins <blevins.charlie@gmail.com>
2017-05-22 08:58:43 -0400

    add ios messenger to registration page

--- public/scripts/messenger.js
+++ public/scripts/messenger.js
@@ -15,16 +15,40 @@
 
 		if (!window.webkit.messageHandlers.signInClicked) return false
 
-		var username = document.querySelector('input[name="username"]').value;
-		var password = document.querySelector('input[name="password"]').value;
+        var cred = getCredVals();
+
+        if (!cred) return;
 
 		var messageToPost = {
 			'action': 'loginAttempt',
-			'username': username,
-			'password': password
+			'username': cred.username,
+			'password': cred.password
 		};
 		window.webkit.messageHandlers.signInClicked.postMessage(messageToPost);
 	}
+
+    function getCredVals () {
+
+		var username;
+        
+        // login
+        if (document.querySelector('input[name="username"]')) {
+            username = document.querySelector('input[name="username"]').value;
+
+        // new account registration
+        } else {
+            username = document.querySelector('input[name="email"]').value;
+        }
+
+        var password = document.querySelector('input[name="password"]').value;
+
+        if (!username || !password) return false;
+
+        return {
+            username: username,
+            password: password
+        };
+    }
 
 	/**
 	 * Returns true if in ios, false if not
